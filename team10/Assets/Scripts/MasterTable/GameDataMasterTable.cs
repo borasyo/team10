40c7d7ef413783b048686dda49267a63fe21cfab
Syota Obora <bora00940@gmail.com>
2017-09-04 14:24:52 +0900

    【Scripts】 csvのロード処理実装

---
+++ team10/Assets/Scripts/MasterTable/GameDataMasterTable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDataMasterTable : MasterTableBase<GameDataMaster>
+{
+    private static readonly string FilePath = "GameData";
+    public void Load() { Load(FilePath); }
+}
+
+public class GameDataMaster : MasterBase
+{
+    public override void Load(string[] elements)
+    {
+        int id = 0;
+        int.TryParse(elements[0], out id);
+        ID = id;
+
+        int eventId = 0;
+        int.TryParse(elements[1], out eventId);
+        EventID = eventId;
+
+        Name = elements[2];
+        Text = elements[3];
+    }
+
+    public int ID { get; private set; }         // 連番で割り振られるユニークな番号
+    public int EventID { get; private set; }    // 生成するイベントID 
+    public string Name { get; private set; }    // 話すキャラの名前 
+    public string Text { get; private set; }    // 話すテキスト
+}
---
+++ team10/Assets/Scripts/MasterTable/MasterTableBase.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+public class MasterTableBase<T> where T : MasterBase, new()
+{
+    protected List<T> masters;
+    public List<T> All { get { return masters; } }
+
+    public void Load(string filePath)
+    {
+        var text = ((TextAsset)Resources.Load(filePath, typeof(TextAsset))).text;
+        text = text.Trim().Replace("\r", "") + "\n";
+        var lines = text.Split('\n').ToList();
+
+        // header
+        var headerElements = lines[0].Split(',');
+        lines.RemoveAt(0); // header
+
+        // body
+        masters = new List<T>();
+        foreach (var line in lines)
+        {
+            ParseLine(line, headerElements);
+        }
+    }
+
+    private void ParseLine(string line, string[] headerElements)
+    {
+        var elements = line.Split(',');
+        if (elements.Length == 1)
+            return;
+
+        if (elements.Length != headerElements.Length)
+        {
+            Debug.LogWarning(string.Format("can't load: {0}", line));
+            return;
+        }
+
+        var master = new T();
+        master.Load(elements);
+        masters.Add(master);
+    }
+}
+
+public class MasterBase
+{
+    public virtual void Load(string[] elements)
+    {
+        // 継承先で記述する
+    }
+}
