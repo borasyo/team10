74a9355afe3b99e5ce003623c657a079628a9161
Syota Obora <bora00940@gmail.com>
2017-09-07 18:34:46 +0900

    テキストアニメーション実装

---
+++ team10/Assets/Scripts/TextAnimator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAnimator : MonoBehaviour
+{
+    // 表示用Text
+    [SerializeField] Text _text = null;
+
+    // 表示間隔
+    [SerializeField] float _interval = 0.1f;
+
+    // SEがあれば再生
+    [SerializeField] AudioSource _se = null;
+
+    /// <summary>
+    /// アニメーション実行
+    /// </summary>
+    public IEnumerator Run(string message)
+    {
+        int loopCount = 0;
+        while(loopCount < message.Length)
+        {
+            // テキストを更新
+            _text.text = message.Substring(0, loopCount);
+
+            // SEあれば再生
+            if(_se)
+                _se.Play();
+
+            // 指定秒数待ち、カウントを増加
+            yield return new WaitForSeconds(_interval);
+            loopCount ++;
+        }
+
+        yield break;
+    }
+}
