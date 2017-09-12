14fa70b2f5c27d7c6cbe350a30a14bb0f8409ad3
Syota Obora <bora00940@gmail.com>
2017-09-12 17:52:22 +0900

    csvLoadとLoadしたデータによるゲームの進行を実装

---
+++ team10/Assets/Scripts/Event/IineEffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IineEffect : EventBase
+{
+    Sprite _sprite = null;
+    ParticleSystem _effect = null;
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    void Start ()
+    {
+        CharacterManager.Instance.ChangeAtackChara(_name);
+        transform.position = CharacterManager.Instance.GetCharacter(_name).position;
+        _effect = GetComponentInChildren<ParticleSystem>();
+	}
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update ()
+    {
+        if (_effect.isPlaying)
+            return;
+
+        // 終了
+        CharacterManager.Instance.ChangeIdleChara(_name);
+        EventEnd = true;
+    }
+}
---
+++ team10/Assets/Scripts/MasterTable/GameDataMasterTable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDataMasterTable : MasterTableBase<GameDataMaster>
+{
+    private static readonly string FilePath = "GameData";
+    public void Load() { Load(FilePath); }
+}
+
+public class GameDataMaster : MasterBase
+{
+    public override void Load(string[] elements)
+    {
+        Name = elements[0];
+        Message = elements[1];
+
+        int eventId = 0;
+        int.TryParse(elements[2], out eventId);
+        EventID = eventId;
+    }
+    
+    public string  Name    { get; private set; }    // 話すキャラの名前 
+    public string  Message { get; private set; }    // 話すテキスト
+    public int     EventID { get; private set; }    // 生成するイベントID 
+}
