7ba041d3f6c0451f1941dad96f7c631649d3775a
Syota Obora <bora00940@gmail.com>
2017-09-14 12:25:33 +0900

    ぼらエフェクト仮作成

---
+++ team10/Assets/Scripts/Effect/AllAtack.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AllAtack : EventBase
+{
+    // イベントプレハブのパス、{0}にはEventIDを入れる
+    const string EventPrefabPath = "Prefabs/Event/eventID_{0}";
+
+    [SerializeField] float _interval = 0.5f;
+
+    /// <summary>
+    /// 生成時処理
+    /// </summary>
+    private void Start()
+    {
+        StartCoroutine(Run());
+    }
+
+    /// <summary>
+    /// エフェクト再生
+    /// </summary>
+    private IEnumerator Run()
+    { 
+        List<Coroutine> _allList = new List<Coroutine>();
+
+        _allList.Add(StartCoroutine(PlayEffect(GameDefine.Ran, 0)));
+        yield return new WaitForSeconds(_interval);
+        _allList.Add(StartCoroutine(PlayEffect(GameDefine.Kuririn, 1)));
+        yield return new WaitForSeconds(_interval);
+        _allList.Add(StartCoroutine(PlayEffect(GameDefine.Yukka, 3)));
+        yield return new WaitForSeconds(_interval);
+        _allList.Add(StartCoroutine(PlayEffect(GameDefine.Kouchan, 2)));
+
+        // 全て終了するまで待つ
+        while (_allList.Count > 0)
+        {
+            yield return _allList[0];
+            _allList.RemoveAt(0);
+        }
+
+        EventEnd = true;
+    }
+
+    private IEnumerator PlayEffect(string charaName, int id)
+    {
+        var eventPrefab = Resources.Load(string.Format(EventPrefabPath, id)) as GameObject;
+        var eventBase = Instantiate(eventPrefab).GetComponent<EventBase>();
+        eventBase.Init(charaName);
+        yield return new WaitWhile(() => !eventBase.EventEnd);
+        Destroy(eventBase.gameObject);
+    }
+}
