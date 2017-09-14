ad3c93ad4e1e198c935adbcc4c199efdff19fd2d
Syota Obora <bora00940@gmail.com>
2017-09-14 12:24:28 +0900

    こうちゃんエフェクト作成

---
+++ team10/Assets/Scripts/Effect/BezierCurve.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BezierCurve : MonoBehaviour
+{
+	public struct tBez
+    {
+		public float t;				// 時間
+		public Vector3 b0,b1,b2;	// 点
+	};
+
+    /// <summary>
+    /// 現在座標の算出
+    /// </summary>
+    static public Vector3 CulcBez(tBez bez)
+    {
+        if (bez.t > 1.0f)
+            bez.t = 1.0f;
+
+		float x = (1 - bez.t) * (1 - bez.t) * bez.b0.x + 2 * (1 - bez.t) * bez.t * bez.b1.x + bez.t * bez.t * bez.b2.x;
+		float y = (1 - bez.t) * (1 - bez.t) * bez.b0.y + 2 * (1 - bez.t) * bez.t * bez.b1.y + bez.t * bez.t * bez.b2.y;
+		return new Vector3 (x, y, 0.0f);
+	}
+
+    /// <summary>
+    /// 現在座標の算出（Z軸含む）
+    /// </summary>
+    static public Vector3 CulcBez(tBez bez, bool bZ)
+    {
+        if (bez.t > 1.0f)
+            bez.t = 1.0f;
+
+        float x = (1 - bez.t) * (1 - bez.t) * bez.b0.x + 2 * (1 - bez.t) * bez.t * bez.b1.x + bez.t * bez.t * bez.b2.x;
+		float y = (1 - bez.t) * (1 - bez.t) * bez.b0.y + 2 * (1 - bez.t) * bez.t * bez.b1.y + bez.t * bez.t * bez.b2.y;
+		float z = (1 - bez.t) * (1 - bez.t) * bez.b0.z + 2 * (1 - bez.t) * bez.t * bez.b1.z + bez.t * bez.t * bez.b2.z;
+		return new Vector3 (x, y, z);
+	}
+}
---
+++ team10/Assets/Scripts/Effect/SoccerEffect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerEffect : EventBase
+{
+    [SerializeField] float _speed = 2.0f;
+    [SerializeField] int _rotAmount = 36;
+    [SerializeField] Vector3 _curve = new Vector3(5, 5, 0);
+
+    [SerializeField] GameObject _ball = null;
+    [SerializeField] GameObject _fireEffectPrefab = null;
+
+    /// <summary>
+    /// 生成時処理
+    /// </summary>
+    private void Start()
+    {
+        StartCoroutine(Run());
+    }
+
+    /// <summary>
+    /// エフェクト再生
+    /// </summary>
+    private IEnumerator Run()
+    {
+        // Ballを飛ばす処理
+        var Bezier = new BezierCurve.tBez();
+        Bezier.t = 0.0f;
+        Bezier.b0 = transform.position;
+        Bezier.b1 = transform.position + _curve;
+        Bezier.b2 = CharacterManager.Instance.GetCharacter(GameDefine.Soyaman).position;
+
+        while (Bezier.t < 1.0f)
+        {
+            Bezier.t += Time.deltaTime * _speed;
+            _speed += _speed * 0.1f;
+            transform.position = BezierCurve.CulcBez(Bezier);
+            transform.eulerAngles += new Vector3(0, 0, _rotAmount);
+            yield return null;
+        }
+
+        // 爆発エフェクト再生
+        Destroy(_ball);
+        ParticleSystem healEffect = Instantiate(_fireEffectPrefab, transform.position, Quaternion.identity).GetComponent<ParticleSystem>();
+        healEffect.transform.SetParent(transform);
+        yield return new WaitWhile(() => healEffect.isPlaying);
+
+        // 終了
+        EventEnd = true;
+    }
+}
