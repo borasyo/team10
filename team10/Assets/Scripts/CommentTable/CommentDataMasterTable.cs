901e7c5f74ae88280ee8738cc272b922a01aeef7
ryosebach <ryo.freezia@gmail.com>
2017-09-21 03:47:46 +0900

    コメントの読み込み機能の実装

---
+++ team10/Assets/Scripts/CommentTable/CommentDataMasterTable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class CommentDataMasterTable : CommentTableBase<CommentDataMaster>
+{
+	private static readonly string URL = 
+        "https://docs.google.com/spreadsheets/d/1ocMc9OAxXa8hUTciVw5ocFMEBBI6XOPHoMG8qkvapqI/export?format=csv";
+
+	private IObservable<Unit> _loadObservable;
+    /// <summary>
+    /// ロードの完了を通知してくれるObservar．
+    /// Subscribeすれば完了後に実行される．
+    /// </summary>
+    /// <value>The load observable.</value>
+	public IObservable<Unit> LoadObservable
+	{
+		get
+		{
+            return _loadObservable ?? (_loadObservable = Observable.FromCoroutine(() => LoadAsyncCorutine(URL)).PublishLast().RefCount());
+		}
+	}
+}
+
+public class CommentDataMaster : MasterBase
+{
+    public override void Load(string[] elements)
+    {
+        ActionID = int.Parse(elements[0]);
+        TimeToFlow = float.Parse(elements[1]);
+        FlowingTime = float.Parse(elements[2]);
+        Comment = elements[3];
+        CommentColor = ColorParser.ToColorOrWhite(elements[4]);
+        IsBold = bool.Parse(elements[5]);
+    }
+    public int ActionID { get; private set; }
+    public float TimeToFlow { get; private set; } //流れるのにかかる時間
+    public float FlowingTime { get; private set; } //アクション開始からコメントが流れるまでの時間
+    public string Comment { get; private set; } 
+    public Color CommentColor { get; private set; } //コメントの色
+    public bool IsBold { get; private set; } //太字にするかどうか
+}
---
+++ team10/Assets/Scripts/CommentTable/CommentTableBase.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UniRx;
+
+
+public class CommentTableBase<T> where T : MasterBase, new()
+{
+    protected List<T> masters;
+    public List<T> All { get { return masters; } }
+
+    /// <summary>
+    /// データの読み込み．
+    /// wwwアクションを伴うので非同期で行う．
+    /// </summary>
+    /// <returns>The async corutine.</returns>
+    /// <param name="URL">URL.</param>
+    protected IEnumerator LoadAsyncCorutine(string URL)
+    {
+        var www = new WWW(URL);
+        yield return www;
+
+        var text = www.text;
+
+		text = text.Trim().Replace("\r", "") + "\n";
+		var lines = text.Split('\n').ToList();
+
+		// header
+		var headerElements = lines[0].Split(',');
+		lines.RemoveAt(0); // header
+
+		// body
+		masters = new List<T>();
+		foreach (var line in lines)
+		{
+			ParseLine(line, headerElements);
+		}
+
+        yield return null;
+    }
+
+    // MasterTableBaseと同じメソッドなのでなんとかしたい
+    private void ParseLine(string line, string[] headerElements)
+    {
+        var elements = line.Split(',');
+        if (elements.Length == 1)
+            return;
+
+        if (elements.Length != headerElements.Length)
+        {
+            Debug.LogWarning(string.Format("can't load: {0}", line));
+            return;
+        }
+
+        var master = new T();
+        master.Load(elements);
+        masters.Add(master);
+    }
+}
